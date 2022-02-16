7e252946a2ce0bbd00769220515f3718a4bff2a1
KwonTaeyong <xodyd2313757@naver.com>
2022-02-16 14:07:48 +0900

    instagram

---
+++ photo/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Photo
+
+
+# Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'created', 'updated']
+    raw_id_fields = ['author']
+    list_filter = ['created', 'updated', 'author']
+    search_fields = ['text', 'created']
+    ordering = ['-updated', '-created']
+
+
+admin.site.register(Photo, PhotoAdmin)
---
+++ photo/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from .models import Photo
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+# Create your views here.
+@login_required
+def photo_list(request):
+    photos = Photo.objects.all()
+    return render(request, 'photo/list.html', {'photos': photos})
+
+
+class PhotoUploadViews(LoginRequiredMixin, CreateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/upload.html'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form': form})
+
+
+class PhotoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Photo
+    success_url = '/'
+    template_name = 'photo/delete.html'
+
+
+class PhotoUpdateView(LoginRequiredMixin, UpdateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/update.html'
